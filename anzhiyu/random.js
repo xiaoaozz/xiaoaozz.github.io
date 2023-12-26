9b425175ce09a3b41a11af0f52299b8375373743
zal <3189137314@qq.com>
2023-12-26 21:54:48 +0800

    Site updated: 2023-12-26 21:54:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/25/MySQL联合索引最左匹配原则详解/"];function toRandomPost(){
+var posts=["2023/12/26/Java基础篇/","2023/12/25/MySQL联合索引最左匹配原则详解/","2023/12/26/MySQL面经篇/","2023/12/26/Java面经篇/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
